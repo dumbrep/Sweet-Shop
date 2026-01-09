b10dcda3f203e8cf53270fcce976771e397c43d8
dumbrep <dumbreprajwal@gmail.com>
2026-01-09 15:41:45 +0530

    logout to logout2

--- sweet-shop-frontend/src/components/Layout/Navbar.jsx
+++ sweet-shop-frontend/src/components/Layout/Navbar.jsx
@@ -29,7 +29,7 @@ const Navbar = () => {
           </div>
           <button onClick={handleLogout} style={styles.logoutBtn}>
             <LogOut size={18} />
-            Logout 
+            Logout2
           </button>
         </div>
       </div>
